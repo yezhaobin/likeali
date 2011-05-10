fe8758177e1425b88c268d234e497a699b4ccf65
yezhaobin <yezhaobin@gmail.com>
2011-05-10 17:32:23 +0800

    start write admin

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -20,4 +20,11 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+	
+	function init(){
+		parent::init();
+		if(isset($_GET["lang"])&&($_GET["lang"]== "en" || $_GET["lang"] =="zh_cn")){
+			Yii::app()->setLanguage($_GET["lang"]);
+		}
+	}
 }
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -3,16 +3,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/global.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
@@ -28,11 +19,11 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				array('label'=>Yii::t("top_nav","Brands"), 'url'=>array('/admin/brands')),
+				array('label'=>Yii::t("top_nav", "Agents"), 'url'=>array('/admin/agents')),
+				array('label'=>Yii::t("top_nav","Accouts"), 'url'=>array('/admin/accounts')),
+				array('label'=>Yii::t("top_nav","Login"), 'url'=>array('/admin/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label'=>Yii::t("top_nav","Logout").'('.Yii::app()->user->name.')', 'url'=>array('/admin/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
 	</div><!-- mainmenu -->
@@ -45,9 +36,8 @@
 	<?php echo $content; ?>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by figo Company.<br/>
 		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
 
 </div><!-- page -->
